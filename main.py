f79ff26d85b6be3a2153374609611792e29d4820
matheuspedrosa13 <matheuspmc13@gmail.com>
2023-02-24 11:46:35 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Third-Party Library
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Project
+from src.services.router.service import Router
+import src.routes.trainer.route
+
+app = FastAPI(root_path="/")
+
+app.include_router(Router.get_router())
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    port = 9999
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        access_log=True,
+        root_path="/",
+    )
